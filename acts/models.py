a9c74b3bc6c6a65f09498f7443a77fc1982c40bc
yuyanghh <fisheepx@gmail.com>
2021-05-09 23:39:54 +0800

    update data structure

--- acts/models.py
+++ acts/models.py
@@ -1,6 +1,49 @@
 from django.db import models
+from datetime import date
 
 
-class Act(models.Model):
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Act(TimeStampMixin):
     id = models.AutoField(auto_created=True, primary_key=True)
-    name = models.CharField(max_length=500)
+    name = models.CharField(max_length=500, unique=True)  # 法案名稱
+    act_type = models.CharField(max_length=30, blank=True)  # 位階
+    legislated_at = models.DateField(blank=True)  # 制定日期
+    amended_at = models.DateField(blank=True)  # 修正日期
+    announced_at = models.DateField(blank=True)  # 公佈日期
+    applied_at = models.DateField(blank=True)  # 施行日期
+    rich_content = models.TextField(unique=True, blank=True)  # 現行法案全文
+    keyword = models.TextField(blank=True)  # 現行法案全文關鍵字
+
+
+class Article(TimeStampMixin):
+    id = models.AutoField(auto_created=True, primary_key=True)
+    rich_content = models.TextField(blank=True, unique=True)  # 現行法條全文
+    act = models.ForeignKey('Act', on_delete=models.CASCADE)  # 所屬法案
+    chapter_nr = models.IntegerField(blank=True)  # 章節
+    article_nr = models.IntegerField()  # 條目
+    legislated_at = models.DateField(blank=True)  # 制定日期
+    amended_at = models.DateField(blank=True)  # 修正日期
+    announced_at = models.DateField(blank=True)  # 公佈日期
+    applied_at = models.DateField(blank=True)  # 施行日期
+    keyword = models.TextField(blank=True)  # 現行法條全文關鍵字
+    updated_reason = models.TextField(blank=True)  # 條文修正/制定理由
+    deliberation_duration = models.IntegerField(blank=True)  # 立法歷程天數
+    appended_decision = models.TextField(blank=True)  # 附帶決議連結
+
+
+class Legislation(TimeStampMixin):
+    id = models.AutoField(auto_created=True, primary_key=True)
+    article = models.ForeignKey('Article', on_delete=models.CASCADE)  # 所屬法條
+    procedure_schedule = models.CharField(
+        max_length=30)  # 議案進度(一讀、二讀、委員會審查、三讀...)
+    sittings_date = models.DateField()  # 會議日期
+    gazette = models.TextField(blank=True)  # 立法院公報紀錄連結
+    main_proposer = models.CharField(max_length=30, blank=True)  # 主提案
+    related_doc = models.TextField(blank=True)  # 關係文書連結
--- acts/serializers.py
+++ acts/serializers.py
@@ -1,8 +1,22 @@
 from acts.models import Act
+from acts.models import Article
+from acts.models import Legislation
 from rest_framework import serializers
 
 
 class ActSerializer(serializers.ModelSerializer):
     class Meta:
         model = Act
+        fields = '__all__'
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class LegislationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Legislation
         fields = '__all__'
