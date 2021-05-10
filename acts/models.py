8ba52c5efdca85231654b7bed85f0fd328029b71
yuyanghh <fisheepx@gmail.com>
2021-05-10 16:48:31 +0800

    add act crawler

--- acts/models.py
+++ acts/models.py
@@ -13,11 +13,14 @@ class TimeStampMixin(models.Model):
 class Act(TimeStampMixin):
     id = models.AutoField(auto_created=True, primary_key=True)
     name = models.CharField(max_length=500, unique=True)  # 法案名稱
+    pcode = models.CharField(max_length=30, unique=True, blank=True)  # 法案Pcode
+    valid_state = models.CharField(
+        max_length=300, unique=True, blank=True)  # 法案生效狀態
     act_type = models.CharField(max_length=30, blank=True)  # 位階
-    legislated_at = models.DateField(blank=True)  # 制定日期
-    amended_at = models.DateField(blank=True)  # 修正日期
-    announced_at = models.DateField(blank=True)  # 公佈日期
-    applied_at = models.DateField(blank=True)  # 施行日期
+    legislated_at = models.DateField(blank=True, null=True)  # 制定日期
+    amended_at = models.DateField(blank=True, null=True)  # 修正日期
+    announced_at = models.DateField(blank=True, null=True)  # 公佈日期
+    applied_at = models.DateField(blank=True, null=True)  # 施行日期
     rich_content = models.TextField(unique=True, blank=True)  # 現行法案全文
     keyword = models.TextField(blank=True)  # 現行法案全文關鍵字
 
@@ -29,10 +32,10 @@ class Article(TimeStampMixin):
                             on_delete=models.CASCADE)  # 所屬法案
     chapter_nr = models.IntegerField(blank=True)  # 章節
     article_nr = models.IntegerField()  # 條目
-    legislated_at = models.DateField(blank=True)  # 制定日期
-    amended_at = models.DateField(blank=True)  # 修正日期
-    announced_at = models.DateField(blank=True)  # 公佈日期
-    applied_at = models.DateField(blank=True)  # 施行日期
+    legislated_at = models.DateField(blank=True, null=True)  # 制定日期
+    amended_at = models.DateField(blank=True, null=True)  # 修正日期
+    announced_at = models.DateField(blank=True, null=True)  # 公佈日期
+    applied_at = models.DateField(blank=True, null=True)  # 施行日期
     keyword = models.TextField(blank=True)  # 現行法條全文關鍵字
     updated_reason = models.TextField(blank=True)  # 條文修正/制定理由
     deliberation_duration = models.IntegerField(blank=True)  # 立法歷程天數
--- acts/views.py
+++ acts/views.py
@@ -1,9 +1,24 @@
+# coding=UTF-8
 from django.http import JsonResponse
 from django.db import transaction
 from rest_framework.generics import GenericAPIView
 
 from acts.serializers import ActSerializer
 from acts.models import Act
+
+import datetime
+import json
+import requests as rq
+from bs4 import BeautifulSoup
+import pandas as pd
+
+# segment chinese
+import jieba
+import jieba.analyse
+# import jieba.analyse
+jieba.set_dictionary('./jieba_dict/include_dict.txt')
+jieba.load_userdict('./jieba_dict/cultural_laws_dict.txt')
+exclude_dict = './jieba_dict/exclude_dict.txt'
 
 
 class ActView(GenericAPIView):
@@ -18,12 +33,96 @@ class ActView(GenericAPIView):
 
     def post(self, request, *args, **krgs):
         data = request.data
+        result = {}
+
+        # crawler methods
+        def parse_chinese_date(date_string):
+            chinese_year = int(date_string.split('民國 ')[1].split(' 年')[0])
+            chinese_month = int(date_string.split('年 ')[1].split(' 月')[0])
+            chinese_day = int(date_string.split('月 ')[1].split(' 日')[0])
+            return datetime.date(chinese_year+1911, chinese_month, chinese_day)
+
+        def check_self_with_classname(self, classname):
+            if(type(self).__name__ == 'Tag'):
+                return self.has_attr('class') and classname in self['class']
+            else:
+                return False
+
+        def remove_stop_words(keyword_list):
+            with open(exclude_dict, 'r') as f:
+                stop_words = f.readlines()
+            stop_words = [stop_word.rstrip() for stop_word in stop_words]
+            new_keyword_list = []
+            for keyword in keyword_list:
+                if keyword not in stop_words:
+                    new_keyword_list.append(keyword)
+            return list(filter(lambda x: x != '' and x != ' ', new_keyword_list))
+
+        def segment_keyword(text, cut_all=False):
+            return remove_stop_words(jieba.lcut(text, cut_all=cut_all))
+
+        def count_keyword_freq(keyword_list):
+            keyword_df = pd.DataFrame(keyword_list, columns=['keyword'])
+            keyword_df['count'] = 1
+            keyword_freq = keyword_df.groupby(
+                'keyword')['count'].sum().sort_values(ascending=False)
+            keyword_freq = pd.DataFrame(keyword_freq)
+            return json.dumps(keyword_freq.to_records(index=True).tolist(), separators=(',', ':'))
+
         try:
-            serializer = self.serializer_class(data=data)
+            result['name'] = act_name = data['name']  # db
+            act_query_response = rq.get(
+                'https://law.moj.gov.tw/Law/LawSearchResult.aspx?ty=ONEBAR&kw='+act_name)
+            act_query_html_doc = act_query_response.text
+            act_query_soup = BeautifulSoup(act_query_html_doc, 'html.parser')
+            act_ref_url = act_query_soup.find_all(
+                'a', id='hlkLawLink', title=act_name)[0].get('href')
+            act_pcode = act_ref_url.split('?')[1][6:14]
+            result['pcode'] = act_pcode  # db
+            act_url = 'https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=' + act_pcode
+
+            act_response = rq.get(act_url)
+            act_html_doc = act_response.text
+            act_soup = BeautifulSoup(act_html_doc, 'html.parser')
+
+            act_amended_date = act_soup.find(id='trLNNDate')
+            if act_amended_date:
+                act_amended_date = act_amended_date.find('td').string
+                result['amended_at'] = parse_chinese_date(act_amended_date)
+
+            act_announced_date = act_soup.find(id='trLNODate')
+            if act_announced_date:
+                act_announced_date = act_announced_date.find('td').string
+                result['announced_at'] = parse_chinese_date(act_announced_date)
+
+            act_valid_state = act_soup.select('#VaildState > td')[0]
+            if act_valid_state:
+                result['valid_state'] = act_valid_state.get_text()
+
+            act_type = act_soup.find('table').find_all(
+                'tr')[-1].find('td').string
+            result['act_type'] = ', '.join(act_type.split(' ＞ '))
+
+            act_content_list = act_soup.find(
+                'div', class_='law-reg-content').children
+            act_rich_content = ''
+            for act_content in act_content_list:
+                if check_self_with_classname(act_content, 'char-2'):
+                    act_rich_content = act_rich_content + act_content.string + '<br>'
+                elif check_self_with_classname(act_content, 'row'):
+                    article_nr = act_content.select('.col-no > a')[0].string
+                    act_rich_content = act_rich_content + article_nr + ' ' + act_content.select(
+                        '.col-data')[0].get_text()
+            keyword_list = segment_keyword(act_rich_content)
+            keyword_freq = count_keyword_freq(keyword_list)
+            result['rich_content'] = act_rich_content
+            result['keyword'] = keyword_freq
+
+            serializer = self.serializer_class(data=result)
             serializer.is_valid(raise_exception=True)
             with transaction.atomic():
                 serializer.save()
-            data = serializer.data
+            result = serializer.data
         except Exception as e:
-            data = {'error': str(e)}
-        return JsonResponse(data)
+            result = {'error': str(e)}
+        return JsonResponse(result)
